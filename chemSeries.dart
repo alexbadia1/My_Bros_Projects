ecea1d6661aa774c513790d21c6936a818e56db7
Alex Badia <badia.alex.h@gmail.com>
2020-05-06 09:09:47 -0400

    Forgot to upload the entire project!

---
+++ chemSeries.dart
@@ -0,0 +1,24 @@
+/**
+ * File: chemSeries.dart is a program that calculates some chem series
+ * Purpose: I intend to make some improvements to my brothers code and provide 
+ * him with an application that will actually implement his chem calculations.
+ * Original Author: Matthew Badia (my brother)
+ * Coded and Implemented by: Alex Badia
+ */
+
+//Creat variable to save the users input
+class LymanSeries {
+
+  double _nInitial;
+  double _nFinal;
+  double energy;
+  double lambda;
+  double frequency;
+
+  void setnInitial (newNinitial) {
+    _nInitial = newNInitial;
+  }//setnInitial
+
+}
+int number = -1;
+
